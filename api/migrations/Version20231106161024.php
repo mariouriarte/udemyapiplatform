b318845dcf7ea05c03905e859eed05d06f9195b7
Mario Uriarte <mario.uriarte@gmail.com>
2023-11-06 13:56:29 -0400

    Adición de lexik jwt authentication

--- api/migrations/Version20231106161024.php
+++ api/migrations/Version20231106161024.php
@@ -11,7 +11,7 @@ final class Version20231106161024 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create User table';
     }
 
     public function up(Schema $schema): void
